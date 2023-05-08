9d2c6606966e1271149a1431c0a92390658758e3
Saphal Sherchan <saphal@outlook.sg>
2023-05-08 19:07:18 +0545

    creating component

--- todo-plan/src/app/app.module.ts
+++ todo-plan/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskPlannerComponent } from './task-planner/task-planner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskPlannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ todo-plan/src/app/task-planner/task-planner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-planner',
+  templateUrl: './task-planner.component.html',
+  styleUrls: ['./task-planner.component.css']
+})
+export class TaskPlannerComponent {
+
+}
